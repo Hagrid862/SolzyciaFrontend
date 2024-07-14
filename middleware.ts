33a9de5560577844b57a10ba5b61e075a5ed9cc9
hagrid862 <n.okroj@icloud.com>
2024-07-14 11:17:37 +0200

    fix: fixed middleware not correctly verifying token

--- middleware.ts
+++ middleware.ts
@@ -1,27 +1,29 @@
+'use server';
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 import axios from 'axios'
 import { cookies } from 'next/headers'
 
 export async function middleware(req: NextRequest) {
-  const loginUrl = req.nextUrl.clone()
-  loginUrl.pathname = '/admin-area'
-  const url = req.nextUrl.clone()
+  const baseUrl = `${req.nextUrl.protocol}//${req.nextUrl.host}`;
+  const loginUrl = new URL('/admin-area', baseUrl);
+  const url = req.nextUrl.clone();
 
   if (
     url.pathname === '/admin-area' ||
     url.pathname === '/admin-area/verify' ||
     url.pathname === '/admin-area/forgot-password'
   ) {
-    return NextResponse.next()
+    return NextResponse.next();
   }
 
   try {
-    const cookieStore = cookies()
-    const access = cookieStore.get('access')
+    const cookieStore = cookies();
+    const access = cookieStore.get('access');
+    const refresh = cookieStore.get('refresh');
     if (!access) {
-      console.log('no access token')
-      return NextResponse.redirect('/admin-area')
+      console.log('no access token');
+      return NextResponse.redirect(loginUrl);
     }
 
     const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/admin/token/verify`, {
@@ -29,45 +31,51 @@ export async function middleware(req: NextRequest) {
       headers: {
         Authorization: `Bearer ${access}`
       }
-    })
+    });
+
+    console.log("verify status: " + response.status);
 
     if (response.status !== 200) {
-      return NextResponse.redirect('/admin-area')
+      console.log('invalid access token');
+      return NextResponse.redirect(loginUrl);
     }
 
-    const data = await response.json()
+    const data = await response.json();
 
-    if (!data.isValid) {
+    if (data.Status !== 'VALID') {
       const refreshResponse = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/admin/token/refresh`, {
         method: 'POST',
         headers: {
-          Authorization: `Bearer ${access}`
+          Authorization: `Bearer ${refresh}`
         }
-      })
+      });
 
       if (refreshResponse.status !== 200) {
-        return NextResponse.redirect('/admin-area')
+        console.log('invalid refresh token');
+        return NextResponse.redirect(loginUrl);
       }
 
-      const refreshData = await refreshResponse.json()
-
+      const refreshData = await refreshResponse.json();
+      console.log(refreshData);
       if (refreshData.refresh && refreshData.access) {
         cookieStore.set('refresh', refreshData.access, {
           expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30)
-        })
+        });
         cookieStore.set('access', refreshData.access, {
           expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30)
-        })
+        });
         if (
           url.pathname === '/admin-area' ||
           url.pathname === '/admin-area/verify' ||
           url.pathname === '/admin-area/forgot-password'
         ) {
-          return NextResponse.redirect('/admin-area/dashboard')
+          const adminAreaUrl = new URL('/admin-area/dashboard', baseUrl);
+          return NextResponse.redirect(adminAreaUrl);
         }
-        return NextResponse.next()
+        return NextResponse.next();
       } else {
-        return NextResponse.redirect('/admin-area')
+        console.log('no refresh token');
+        return NextResponse.redirect(loginUrl);
       }
     } else {
       if (
@@ -75,20 +83,17 @@ export async function middleware(req: NextRequest) {
         url.pathname === '/admin-area/verify' ||
         url.pathname === '/admin-area/forgot-password'
       ) {
-        const adminAreaUrl = req.nextUrl.clone()
-        adminAreaUrl.pathname = '/admin-area/dashboard'
-        return NextResponse.redirect(adminAreaUrl)
+        const adminAreaUrl = new URL('/admin-area/dashboard', baseUrl);
+        return NextResponse.redirect(adminAreaUrl);
       }
-      return NextResponse.next()
+      return NextResponse.next();
     }
   } catch (e) {
-    console.log(e)
-    const loginUrl = req.nextUrl.clone()
-    loginUrl.pathname = '/admin-area'
-    return NextResponse.redirect(loginUrl)
+    console.log(e);
+    return NextResponse.redirect(loginUrl);
   }
 }
 
 export const config = {
   matcher: '/admin-area/:path*'
-}
+};

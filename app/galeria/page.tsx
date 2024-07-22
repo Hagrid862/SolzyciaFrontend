6e4dd6737e462e8ac1543bf0377ac535a393ecbe
hagrid862 <n.okroj@icloud.com>
2024-07-22 20:03:32 +0200

    feat: created simple gallery based on local files

---
+++ app/galeria/page.tsx
@@ -0,0 +1,84 @@
+'use client'
+
+import image1 from '@/assets/images/games.jpg'
+import image2 from '@/assets/images/jeep-offroad.jpg'
+import image3 from '@/assets/images/knife.jpg'
+import image4 from '@/assets/images/survival.jpg'
+
+import Image from 'next/image'
+import { Card, CardBody, Divider } from '@nextui-org/react'
+import { useState } from 'react'
+
+export default function galleryPage() {
+  const images = [
+    {
+      image: image1,
+      title: 'Games',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, eleifend nunc. Nulla ut turpis nec leo ultricies.'
+    },
+    {
+      image: image2,
+      title: 'Jeep Offroad',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, eleifend nunc. Nulla ut turpis nec leo ultricies.'
+    },
+    {
+      image: image3,
+      title: 'Knife',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, eleifend nunc. Nulla ut turpis nec leo ultricies.'
+    },
+    {
+      image: image4,
+      title: 'Survival',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, eleifend nunc. Nulla ut turpis nec leo ultricies.'
+    }
+  ]
+  const [currentImage, setCurrentImage] = useState(0)
+
+  return (
+    <div className='flex flex-col items-center justify-center'>
+      <Card className='max-w-[1200px] w-full min-h-32 mt-12 h-[calc(100vh-64px-6rem)] max-h-[1200px] flex flex-col'>
+        <div className='w-full flex flex-row gap-3 max-h-[960px] h-4/5'>
+          <div className='w-[calc(75%-0.5rem)] relative p-4'>
+            <Image
+              src={images[currentImage].image}
+              alt='gallery-image'
+              layout='fill'
+              objectFit='contain'
+              className='rounded-lg'
+            />
+          </div>
+          <Divider orientation={'vertical'} />
+          <div className='w-[calc(25%-0.6rem)] overflow-auto flex flex-col gap-2 items-center'>
+            <div />
+            {images.map((image, index) => (
+              <Card
+                isPressable
+                key={index}
+                className={`${currentImage === index ? 'outline-2 outline-[#777777]' : 'outline-0'} aspect-square bg-opacity-15 rounded-lg w-[256px] min-w-[256px] h-[256px] min-h-[256px] overflow-hidden bg-white flex relative`}
+              >
+                <Image
+                  src={image.image}
+                  alt='gallery-image'
+                  layout='fill'
+                  objectFit='contain'
+                  className='rounded-lg cursor-pointer'
+                  onClick={() => setCurrentImage(index)}
+                />
+              </Card>
+            ))}
+            <div />
+          </div>
+        </div>
+        <Divider />
+        <CardBody>
+          <div className='text-3xl font-semibold'>{images[currentImage].title}</div>
+          <div className='text-lg'>{images[currentImage].description}</div>
+        </CardBody>
+      </Card>
+    </div>
+  )
+}

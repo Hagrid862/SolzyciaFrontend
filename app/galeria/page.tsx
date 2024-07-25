448688b4c09615c5189cb20c00e7e394eccad79b
hagrid862 <n.okroj@icloud.com>
2024-07-25 10:16:25 +0200

    feat: added /off-road page and assets to it with real content
    + some tweaks to gallery page

--- app/galeria/page.tsx
+++ app/galeria/page.tsx
@@ -40,7 +40,7 @@ export default function galleryPage() {
 
   return (
     <div className='flex flex-col items-center justify-center'>
-      <Card className='max-w-[1200px] w-full min-h-32 mt-12 h-[calc(100vh-64px-6rem)] max-h-[1200px] flex flex-col'>
+      <Card className='max-w-[1200px] w-full min-h-32 mt-12 h-[calc(100vh-64px-6rem)] max-h-[1200px] hidden flex-col md:flex'>
         <div className='w-full flex flex-row gap-3 max-h-[960px] h-4/5'>
           <div className='w-[calc(75%-0.5rem)] relative p-4'>
             <Image
@@ -52,14 +52,13 @@ export default function galleryPage() {
             />
           </div>
           <Divider orientation={'vertical'} />
-          <div className='w-[calc(25%-0.6rem)] overflow-auto flex flex-col gap-2 items-center'>
+          <div className='w-[calc(25%-0.6rem)] overflow-auto flex flex-col gap-2 items-center overflow-y-auto'>
             <div />
             {images.map((image, index) => (
               <Card
                 isPressable
                 key={index}
-                className={`${currentImage === index ? 'outline-2 outline-[#777777]' : 'outline-0'} aspect-square bg-opacity-15 rounded-lg w-[256px] min-w-[256px] h-[256px] min-h-[256px] overflow-hidden bg-white flex relative`}
-              >
+                className={`${currentImage === index ? 'outline-2 outline-[#777777]' : 'outline-0'} bg-opacity-15 rounded-lg w-[90%] aspect-square overflow-hidden bg-white flex relative h-full min-h-[150px]`}>
                 <Image
                   src={image.image}
                   alt='gallery-image'
